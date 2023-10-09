f5896656303287d2d49fd14f2e4934a19b09421f
Jasper Delight <jasperdelightt@gmail.com>
2023-10-09 15:55:14 -0600

    foundItemCard built out

--- vuePokedex.client/src/components/FoundItemCard.vue
+++ vuePokedex.client/src/components/FoundItemCard.vue
@@ -1,4 +1,15 @@
-<template>{{ foundItem.name }}</template>
+<template>
+  <section class="row">
+    <div class="col-12 fs-3 text-center" style="text-transform: capitalize;">{{ foundItem.name }}</div>
+    <div class="d-flex justify-content-center">
+      <img :src="foundItem.sprites.default" alt="itemIMG" class="" style="height: 50px; width: 50px;" >
+    </div>
+    <div class="col-6 text-center">Cost: {{ foundItem.cost }}</div>
+    <div class="col-6 text-center">Category: {{ foundItem.category.name }}</div>
+    <div class="col-12 text-center">{{ foundItem.flavorTextEntries[0].text }}</div>
+    <div class="col-12 text-center">{{ foundItem.effectEntries[0].effect }}</div>
+  </section>
+</template>
 
 
 <script>
