408680b823024d437143ca01bdb894d63aab8fa5
Jasper Delight <jasperdelightt@gmail.com>
2024-01-11 22:17:24 -0700

    elvis operator added

--- vuePokedex.client/src/components/SavedAbilityCard.vue
+++ vuePokedex.client/src/components/SavedAbilityCard.vue
@@ -3,21 +3,21 @@
     <div class="col-12 fs-3 text-center" style="text-transform: capitalize;"> {{ foundAbility?.name }}</div>
     <div class="col-12 text-center">{{ foundAbility.generation[0]?.name }}</div>
     <div class="col-12 text-center">{{ foundAbility.effectEntries[0]?.effect }}</div>
-    <div class="col-12 fs-3 text-center" style="text-transform: capitalize;"> {{ foundAbility.mainPokemon.name }}</div>
+    <div class="col-12 fs-3 text-center" style="text-transform: capitalize;"> {{ foundAbility.mainPokemon?.name }}</div>
     <section class="col-12 d-flex justify-content-center">
       <img :src="foundAbility.mainPokemon?.img" alt="" class="img-fluid">
     </section>
     <section class="col-4">
-      <span class="fw-bold">Weight: </span>{{ foundAbility.mainPokemon.weight }}
+      <span class="fw-bold">Weight: </span>{{ foundAbility.mainPokemon?.weight }}
     </section>
     <section class="col-4">
-      <span class="fw-bold">Height: </span>{{ foundAbility.mainPokemon.height }}
+      <span class="fw-bold">Height: </span>{{ foundAbility.mainPokemon?.height }}
     </section>
     <section class="col-4">
-      <span class="fw-bold">ID: </span>{{ foundAbility.mainPokemon.id }}
+      <span class="fw-bold">ID: </span>{{ foundAbility.mainPokemon?.id }}
     </section>
     <div class="col-12 fs-3">Base Stats</div>
-    <section class="col-4" v-for="s in foundAbility.mainPokemon.stats" :key="s.stats">
+    <section class="col-4" v-for="s in foundAbility.mainPokemon?.stats" :key="s.stats">
       <span class="fw-bold" style="text-transform: capitalize;">{{ s?.stat?.name }}:</span> {{ s?.base_stat }}
     </section>
   </section>
