2e79f1afce0398e7b449be57836d5cae2df3d5ed
Jasper Delight <jasperdelightt@gmail.com>
2024-01-16 21:17:34 -0700

    ability to save items on backend

--- vuePokedex/server/controllers/ItemsController.js
+++ vuePokedex/server/controllers/ItemsController.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import { itemsService } from "../services/ItemsService";
 import BaseController from "../utils/BaseController";
 
@@ -6,11 +7,21 @@ export class ItemsController extends BaseController{
     super('api/items')
     this.router
     .get('', this.getMyItems)
+    .use(Auth0Provider.getAuthorizedUserInfo)
+    .post('', this.saveItem)
   }
   async getMyItems (req, res, next) {
   try{
       const items = await itemsService.getMyItems()
   return res.send(items)
+  } catch(error) {
+      next(error);
+  }
+  }
+  async saveItem (req, res, next) {
+  try{
+      const item = itemsService.saveItem(req.body)
+  return res.send(item)
   } catch(error) {
       next(error);
   }
--- vuePokedex/server/services/ItemsService.js
+++ vuePokedex/server/services/ItemsService.js
@@ -1,6 +1,11 @@
 import { dbContext } from "../db/DbContext"
 
 class ItemsService{
+  async saveItem(body) {
+    let item = await dbContext.Item.create(body)
+    await item.populate('mainPokemon', 'creatorId')
+    return item
+  }
   async getMyItems() {
     let items = await dbContext.Item.find()
     return items
